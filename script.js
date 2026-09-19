7b45b636f75cf14fa3de9b24ece121ed53014aa2
tanh55900-ai <tanh55900@gmail.com>
2026-09-19 09:41:10 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ function update(PreviewPic) {
     console.log("Mouse over image");
     console.log(PreviewPic.alt);
     console.log(PreviewPic.src);
+    document.getElementById("image").innerHTML = PreviewPic.alt;
 }
